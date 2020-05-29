975ee733f23d6d895490a04b2a4d6e22645caf86
Sanganu <kppkmsanu@gmail.com>
2020-05-29 02:58:14 -0600

    Incorporate localstorage  setitem works

--- version2/scripts.js
+++ version2/scripts.js
@@ -2,4 +2,22 @@ $(".saveinput").on("click",function(){
     let timeblock = $(this).parents("tr").attr("id")
     let userdata = $(this).parents("tr").find("input").val();
     console.log("ENtry",timeblock,userdata)
+    localStorage.setItem(timeblock,userdata)
 })
+
+function displaySchedule(){
+    $(".row").each(()=>{
+        var blockid = $(this).attr("id");
+        console.log(blockid)
+        if( blockid !== "columnhead"){
+        var plan = localStorage.getItem(blockid) || "Enter your Plans here";
+        $(this).children(".userinput").val(plan);
+        console.log(blockid,plan)
+        }
+    })
+    for(let i=9;i<18;i++){
+
+    }
+}
+
+displaySchedule()
