eb06a8c854af40111b8d924229c57d5680dff42a
Sanganu <kppkmsanu@gmail.com>
2020-05-29 09:28:43 -0600

    delete functionality

--- version2/scripts.js
+++ version2/scripts.js
@@ -5,18 +5,25 @@ $(".saveinput").on("click",function(){
     localStorage.setItem(timeblock,userdata)
 })
 
+$(".deleteentry").on("click",function(){
+    let timeblock = $(this).parents("tr").attr("id")
+    localStorage.removeItem("timeblock");
+    let userdata = $(this).parents("tr").find("input").val(" ");
+})
 function displaySchedule(){
     $(".row").each(function(){
         var blockid = $(this).attr("id");
         if( blockid !== "columnhead"){
-            var plan = localStorage.getItem(blockid) || "Enter your Plans here";
-            $(this).find(".userinput").val(plan);
+            var plan = localStorage.getItem(blockid) || "";
+            if (plan){
+                $(this).find(".userinput").val(plan) 
+            }
+            else{
+                 $(this).find(".userinput").attr("placeholder","Enter you schedule here")
+            }
             console.log(blockid,plan)
         }
     })
-    for(let i=9;i<18;i++){
-
-    }
 }
 
 displaySchedule()
