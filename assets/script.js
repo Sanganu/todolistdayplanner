e5c07e7477ce44bdf1b10c931557220f4d8e9c90
Sanganu <kppkmsanu@gmail.com>
2021-01-29 00:58:46 -0700

    Changes using name and class

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,19 @@
-$(".saveBtn").on("click",function(){
-    var value = $(this).attr("id")
-    var time=
+$(".savePlannerEntry").on("click",function(){
+    var timeblock = $(this).attr("name")
+    console.log("name",timeblock)
+    var time= "timer-"+
     localStorage.setItem(time,value);
 })
 var currenthour = moment().hours()
 for(let i=7;i <= 15;i++){
 
+       $(`textarea[name=planner-${i}-userentry]`).val(localStorage.getItem(`time-${i}`))
+    if (currenthour < i){
+        $(`textarea[name=planner-${i}-userentry]`).addClass("bg-warning")
+    }
+    else if(currenthour === i){
+        $(`textarea[name=planner-${i}-userentry]`).addClass("bg-info")
+    }else {
+        $(`textarea[name=planner-${i}-userentry]`).addClass("bg-past")
+    }
 }
