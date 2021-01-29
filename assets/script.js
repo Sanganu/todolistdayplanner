6af9e39334915cf427df5822a577bc6639fac63d
Sanganu <kppkmsanu@gmail.com>
2021-01-28 20:34:42 -0700

    Easyversion upload

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
 $(".saveBtn").on("click",function(){
-    var value = $(this).siblings(".description").val();
-    var time= $(this).parent().attr("id");
+    var value = $(this).attr("id")
+    var time=
     localStorage.setItem(time,value);
 })
+var currenthour = moment().hours()
+for(let i=7;i <= 15;i++){
 
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+}
--- version2/scripts.js
+++ version2/scripts.js
@@ -1,29 +1,50 @@
-$(".saveinput").on("click",function(){
+$(".saveinput").on("click", function () {
     let timeblock = $(this).parents("tr").attr("id")
     let userdata = $(this).parents("tr").find("input").val();
-    console.log("ENtry",timeblock,userdata)
-    localStorage.setItem(timeblock,userdata)
+    console.log("ENtry", timeblock, userdata)
+    localStorage.setItem(timeblock, userdata)
 })
 
-$(".deleteentry").on("click",function(){
+$(".deleteentry").on("click", function () {
     let timeblock = $(this).parents("tr").attr("id")
     localStorage.removeItem("timeblock");
     let userdata = $(this).parents("tr").find("input").val(" ");
 })
-function displaySchedule(){
-    $(".row").each(function(){
-        var blockid = $(this).attr("id");
-        if( blockid !== "columnhead"){
-            var plan = localStorage.getItem(blockid) || "";
-            if (plan){
-                $(this).find(".userinput").val(plan) 
-            }
-            else{
-                 $(this).find(".userinput").attr("placeholder","Enter you schedule here")
-            }
-            console.log(blockid,plan)
+function displaySchedule() {
+    $(".row").each(function () {
+        let blockid = $(this).attr("id");
+        if(blockid !== "tablehead"){
+        let currentblocktime = blockid.split("-")[1]
+        let hour = moment().hours()
+
+        let plan = localStorage.getItem(blockid) || "";
+        if (plan) {
+            $(this).find(".userinput").val(plan)
         }
+        else {
+            $(this).find(".userinput").attr("placeholder", "Enter you schedule here")
+        }
+        console.log(blockid, plan)
+        if (hour < currentblocktime) {
+            $(this).addClass("bg-primary")
+        } else if (hour == currentblocktime) {
+            $(this).addClass("bg-warning")
+        } else {
+            $(this).addClass("bg-danger")
+        }
+    }
+
     })
 }
 
 displaySchedule()
+displayClock()
+
+function displayClock(){
+    let clock = setInterval(ticktick,1000)
+
+    function ticktick(){
+ 
+       $('p[name=currenttime]').text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+    }
+}
